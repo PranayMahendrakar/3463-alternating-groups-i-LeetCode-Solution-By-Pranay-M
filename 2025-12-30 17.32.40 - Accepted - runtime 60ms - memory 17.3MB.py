7c5c2c18e65608f20b3b415b85d496df3f1538d9
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-20 12:59:18 +0530

    Initial commit

---
+++ 2025-12-30 17.32.40 - Accepted - runtime 60ms - memory 17.3MB.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        n = len(colors)
+        count = 0
+        for i in range(n):
+            left = colors[(i - 1) % n]
+            mid = colors[i]
+            right = colors[(i + 1) % n]
+            if mid != left and mid != right:
+                count += 1
+        return count
